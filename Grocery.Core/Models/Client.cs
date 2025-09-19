2582ffca3e10ec3dd0bb9cce26a3cc6ac2092054
dejong39 <sam@zerotop.com>
2025-09-19 19:24:42 +0200

    Implementeer login functionaliteit met Client properties en AuthService
    Client class aangepast om gebruik te maken van properties (EmailAddress, Password)
    
    ClientRepository bijgewerkt zodat de juiste Client wordt opgehaald op basis van emailadres
    
    AuthService uitgebreid met daadwerkelijke password validatie via PasswordHelper
    
    LoginView.xaml toegevoegd aan Grocery.App/Views en geactiveerd in project
    
    Registratie van AuthService en LoginView geactiveerd in MauiProgram
    
    LoginViewModel ingeschakeld in App.xaml.cs
    
    MainPage ingesteld op LoginView i.p.v. AppShell
    
    Route naar Login scherm geactiveerd in AppShell.xaml.cs

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,21 @@
 ﻿
+using Grocery.Core.Helpers;
+
 namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string _emailAddress;
+        public string _emailAddress { get; }
         private string _password { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             _emailAddress=emailAddress;
             _password=password;
+        }
+
+        internal bool verifyPassword(string inputPassword)
+        {
+            return PasswordHelper.VerifyPassword(inputPassword, _password);
         }
     }
 }
